1005ea4129bfcf43345aeadada653c1029f26241
João Carlos Belmiro Gonçalves <joao.goncalves@compasso.com.br>
2022-07-24 21:10:08 -0300

    criando e instanciando layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <Layout />
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Layout from './components/Layout'
+export default {
+  components: {
+    Layout
   }
+}
+</script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+}
+::-webkit-scrollbar {
+  width: 8px;
+}
+::-webkit-scrollbar-thumb {
+  background: $secondary;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: $secondary;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <header>
+      <h2>User <span>Administration</span></h2>
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <h4> © Todos os direitos reservados - <a href="https://github.com/joao-belmiro" target="_blank" rel="noopener noreferrer">GitHub</a> João Carlos - 2022</h4>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Poppins", sans-serif;
+}
+
+header {
+  width: 100%;
+  background-color: $secondary;
+  h2 {
+    color: $grey;
+    line-height: 100px;
+    height: 100px;
+    text-align: left;
+    padding-left: 64px;
+    letter-spacing: 3px;
+
+    span {
+        color: $contrast;
+    }
+    @media screen and (max-width: $small-screen) {
+      padding: 16px;
+    }
+  }
+}
+main {
+    padding: 32px 64px 48px;
+    min-height: 100vh;
+    background-color: $background;
+
+    @media screen and (max-width: $small-screen) {
+      padding: 16px;
+    }
+}
+
+footer {
+    height: 80px;
+    line-height: 80px;
+    background-color: $secondary;
+    text-align: center;
+
+    h4 {
+        color: $grey;
+
+        a {
+            color: $success;
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+      @media screen and (max-width: $small-screen) {
+        font-size: 11px;
+      }
+    }
+}
+</style>
