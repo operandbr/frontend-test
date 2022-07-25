09038ddd386758b88f776baedd5d7ab363ad529f
João Carlos Belmiro Gonçalves <joao.goncalves@compasso.com.br>
2022-07-24 21:21:48 -0300

    criando rotas e store para estado global

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      id: '',
+      fullName: '',
+      userName: '',
+      cpf: '',
+      birthDate: 'nbvgcxz',
+      phoneNumber: ''
+    }
+  },
+  getters: {
+    getUser: (state) => state.user
   },
   mutations: {
+    setUser: (state, user) => {
+      state.user = user
+    }
   },
   actions: {
+    clearUser ({ state }) {
+      const userPropertys = Object.keys(state.user)
+      userPropertys.forEach(property => {
+        state.user[property] = ''
+      })
+    }
   },
   modules: {
   }
